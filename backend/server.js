6e542b7b0a248f7a2b2bb4954e498ef362611e22
kaushik-ak <kaushik.aditya.ak47@gmail.com>
2026-04-03 17:31:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -89,3 +89,7 @@ app.post("/schemes", async (req, res) => {
 // ===== START SERVER =====
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
